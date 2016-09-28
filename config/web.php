ef063b5f7dce4b09b78661ae3340720cea6c4342
apekin-pc\apekin <apekin@uamaster.com>
2016-09-28 12:51:07 +0300

    lesson 1 practic

--- config/web.php
+++ config/web.php
@@ -10,6 +10,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'VltFhbl1U9X2XFzcZ1LSa_7aAlrkd__v',
+            'baseUrl'=>'',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -38,14 +39,13 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                "<action(about|contact|login)>"=>"site/<action>",
             ],
         ],
-        */
     ],
     'params' => $params,
 ];

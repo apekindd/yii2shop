8a4f2f2584da8a1e8840c8fa427fc6e2428b9d82
apekin-pc\apekin <apekin@uamaster.com>
2016-09-30 12:00:29 +0300

    lesson 5 practic

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -3,6 +3,7 @@ namespace app\components;
 
 use yii\base\Widget;
 use app\models\Category;
+use Yii;
 
 class MenuWidget extends Widget
 {
@@ -21,9 +22,17 @@ class MenuWidget extends Widget
     }
 
     public function run(){
+        //get cache
+        $menu = Yii::$app->cache->get('menu');
+        if($menu) return $menu;
+
         $this->data = Category::find()->asArray()->indexBy('id')->all();
         $this->tree = $this->getTree();
         $this->menuHtml = $this->getMenuHtml($this->tree);
+
+        //set cache
+        Yii::$app->cache->set('menu', $this->menuHtml, 60);
+
         return $this->menuHtml;
     }
 

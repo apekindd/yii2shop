13aa4405577f02ed78f0e94be21c862ad053b27f
apekin-pc\apekin <apekin@uamaster.com>
2016-09-30 12:38:40 +0300

    lesson 6 practic branch less6merge

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'basePath' => dirname(__DIR__),
     'language' => 'ru-RU',
     'bootstrap' => ['log'],
+    'defaultRoute' => 'category/index',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation

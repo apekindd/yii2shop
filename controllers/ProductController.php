d9a3ceaa8b9ae62524bd0e03cdb30e763c04891c
apekin-pc\apekin <apekin@uamaster.com>
2016-10-05 12:50:35 +0300

    less 10 practic

---
+++ controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace app\controllers;
+use app\models\Category;
+use app\models\Product;
+use Yii;
+
+class ProductController extends AppController
+{
+    public function actionView($id){
+        $id = Yii::$app->request->get('id');
+
+        //Ленивая
+        $product = Product::findOne($id);
+        //Жадная
+        //$product = Product::find()->with('category')->where(['id'=>$id])->limit(1)->one();
+
+        return $this->render('view', compact('product'));
+    }
+}
